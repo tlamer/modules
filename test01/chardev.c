cbb11aebb269ed71815ae241717a08cb8e679bdc
Peter Kotvan <kotvan@slack.urpi.fei.stuba.sk>
2011-08-10 13:31:00 +0200

    chardev.h added

--- test01/chardev.c
+++ test01/chardev.c
@@ -7,18 +7,9 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>	/* for put_user */
+#include <chardev.h>
 
 MODULE_LICENSE("GPL");
-
-/*  
- *  Prototypes - this would normally go in a .h file
- */
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
-static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS 0
 #define DEVICE_NAME "chardev"	/* Dev name as it appears in /proc/devices   */
